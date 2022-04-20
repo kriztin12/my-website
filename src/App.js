78c447f18f143848377edf43e4d96cb1514d043d
kriztin12 <kriztinmargaret@gmail.com>
2022-04-20 21:50:31 +1000

    sidebar working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+
+//import { Route } from 'react-router-dom';
 import './App.css';
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './components/home';
+import Projects from './components/projects';
+import Sidebar from './components/sidebar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/projects" element={<Projects />}/>
+        </Routes>
+
     </div>
   );
 }
---
+++ src/components/sidebarContents.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+
+export const SideBarContents = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome/>,
+        cName: 'nav-text',
+       
+    }, 
+    {
+        title: 'Projects',
+        path: '/projects',
+        icon: <AiIcons.AiFillHome/>,
+        cName: 'nav-text',
+       
+    }, 
+]
